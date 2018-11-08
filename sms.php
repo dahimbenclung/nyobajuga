42deebf319a2ae2d1cae0d0e773d2c2318cdcfac
dahimbenclung <44826515+dahimbenclung@users.noreply.github.com>
2018-11-08 14:54:07 +0700

    Create sms.php

---
+++ sms.php
@@ -0,0 +1,6 @@
+echo "# nyobajuga" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/dahimbenclung/nyobajuga.git
+git push -u origin master
